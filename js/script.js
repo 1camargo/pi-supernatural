aaa7cf4cf3544c88df702f5362a6bca35f051e15
Gabriel Camargo <camargoxg@gmail.com>
2025-05-18 04:24:41 -0300

    feat: página de login e validações de cadastro

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const elementos = document.querySelectorAll('.fade-in-scroll');
+
+    const observer = new IntersectionObserver((entradas, observer) => {
+      entradas.forEach(entrada => {
+        if (entrada.isIntersecting) {
+          entrada.target.classList.add('visible');
+          observer.unobserve(entrada.target);
+        }
+      });
+    }, {
+      rootMargin: '0px 0px -50px 0px',
+    });
+    elementos.forEach(el => observer.observe(el));
+  });
